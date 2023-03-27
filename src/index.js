248a6ec256d8c54ac8f77262371f4fde834b80c6
Matdev <mcawox@gmail.com>
2023-03-27 22:34:43 +0200

    BrowserRouter back instead of HashRouter

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import { BrowserRouter } from 'react-router-dom';
-import {HashRouter} from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
+//import {HashRouter} from "react-router-dom";
 
 ReactDOM.render(
-    // <BrowserRouter basename={process.env.PUBLIC_URL}>
-    //   <App />
-    // </BrowserRouter>,
-    <HashRouter base="/">
-        <App/>
-    </HashRouter>,
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </BrowserRouter>,
+    // <HashRouter base="/">
+    //     <App/>
+    // </HashRouter>,
     document.getElementById('root')
 );
 
