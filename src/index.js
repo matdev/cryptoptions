d07629f67184ebc4fd5c8254065d671fef31206d
Matdev <mcawox@gmail.com>
2023-03-27 22:46:02 +0200

    HashRouter as Router

--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+//import { BrowserRouter } from 'react-router-dom';
 //import {HashRouter} from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
-ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <App />
-    </BrowserRouter>,
-    // <HashRouter base="/">
-    //     <App/>
-    // </HashRouter>,
-    document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Router>
+        <App />
+    </Router>
 );
+
+// ReactDOM.render(
+//     <BrowserRouter basename={process.env.PUBLIC_URL}>
+//       <App />
+//     </BrowserRouter>,
+//     // <HashRouter base="/">
+//     //     <App/>
+//     // </HashRouter>,
+//     document.getElementById('root')
+// );
 
