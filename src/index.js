c74aab726edacdc2b5f8baf218fb4b166cb2c1b9
Matdev <mcawox@gmail.com>
2023-03-29 22:57:36 +0200

    Using StrictMode

--- src/index.js
+++ src/index.js
@@ -1,31 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-//import {HashRouter} from "react-router-dom";
-//import { HashRouter as Router } from "react-router-dom";
+// import {BrowserRouter} from 'react-router-dom';
+// import {HashRouter} from "react-router-dom";
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//     <Router base="/">
-//         <App />
-//     </Router>
+// https://stackoverflow.com/a/71690259/2068732
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App />);
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
+
+// ReactDOM.render(
+//     <HashRouter basename={process.env.PUBLIC_URL}>
+//         <App/>
+//     </HashRouter>,
+//     document.getElementById('root')
 // );
 
 // ReactDOM.render(
 //     <BrowserRouter basename={process.env.PUBLIC_URL}>
-//       <App />
+//         <App/>
 //     </BrowserRouter>,
-//     // <HashRouter base="/">
-//     //     <App/>
-//     // </HashRouter>,
 //     document.getElementById('root')
 // );
-
-ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <App />
-    </BrowserRouter>,
-    document.getElementById('root')
-);
