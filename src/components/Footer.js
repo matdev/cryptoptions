f29276795f94fe1c8c2e4362f43aaed84fc8f3ae
Matdev <mcawox@gmail.com>
2023-04-21 15:11:54 +0200

    Link to Discord

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,23 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 
 import './Footer.css'
+
 const Footer = () => {
     return (
         <div>
             <h1>
             </h1>
             <p className={'version_tag'}>
-                version 0.6.0
+                version 0.6.1
+            </p>
+
+            <p className={'version_tag'}>
+                Got feedback or feature request ? <br/><br/>
+                Please share it on<Link to="https://discord.gg/wbqzNJhv" target='_blank'> Discord </Link>
+            </p>
+            <p className={'version_tag'}>
+
             </p>
         </div>
     )
