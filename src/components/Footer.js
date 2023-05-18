9122eceb606ffee2c62a514884b9ff8a0e9b54b5
Matdev <mcawox@gmail.com>
2023-05-18 21:32:12 +0200

    CoinGecko reference

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,8 +12,11 @@ const Footer = () => {
                 Curious about this app ? <br/> Any feedback or feature request ? <br/><br/>
                 <b>Join us on<Link to="https://discord.gg/srYeUy9CcR" target='_blank'> Discord </Link> </b>
             </p>
+            <p className={'coingecko_ref'}>
+                Coin prices provided by CoinGecko
+            </p>
             <p className={'version_tag'}>
-                version 0.7.1
+                version 0.7.2
             </p>
         </div>
     )
