7242084e3357c38c75d668f2e205c181ec07ae0d
Matdev <mcawox@gmail.com>
2023-06-22 22:27:17 +0200

    Refactored navbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,7 +16,7 @@ const Footer = () => {
                 Coin prices provided by <Link to="https://www.coingecko.com" target='_blank'> CoinGecko </Link>
             </p>
             <p className={'version_tag'}>
-                version 0.7.8 (beta)
+                version 0.7.9 (beta)
             </p>
         </div>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,18 +11,32 @@ const Navbar = (props) => {
     return (
 
         <div className='navbar'>
-            <Link to='/'>
-                <img src={logo} className="App-logo" alt="CryptOptions"/>
-            </Link>
-            <Link to='/'>
-                <div className='app_name_div'>
-                    <h1 className='app_name'>Crypt<span className='options'>Options</span></h1>
-                    <p className='beta_label hide-mobile'>Beta</p>
-                </div>
-                <p className='app_pitch hide-mobile'>Fair prices of options on cryptocurrencies</p>
-            </Link>
+            <div className='logo_section'>
 
-            <BaseCurrencySelect/>
+                <Link to='/'>
+                    <span>
+                        <img src={logo} className="app_logo" alt="CryptOptions"/>
+                    </span>
+                </Link>
+
+                <Link to='/'>
+                    <div className='app_name_div'>
+                        <h1 className='app_name'>Crypt<span className='options'>Options</span></h1>
+                        <p className='beta_label hide-mobile'>Beta</p>
+                    </div>
+                    <p className='app_pitch hide-mobile'>Fair prices of options on cryptocurrencies</p>
+                </Link>
+
+                <Link className='navbar_link hide-mobile' to='/option-prices/bitcoin'>
+                    <h4>Options on Bitcoin</h4>
+                </Link>
+                <Link className='navbar_link hide-mobile' to='/option-prices/ethereum'>
+                    <h4>Options on Ethereum</h4>
+                </Link>
+            </div>
+            <div>
+                <BaseCurrencySelect/>
+            </div>
         </div>
 
     )
