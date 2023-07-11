337326adc1ca2637f15171446b28bc1c92018d94
Matdev <mcawox@gmail.com>
2023-07-12 00:23:12 +0200

    Add Twitter link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,23 @@
 import React from "react";
 import {Link} from 'react-router-dom';
+import {BsTwitter} from "react-icons/bs";
 
 import './Footer.css'
+import {MdMenu} from "react-icons/md";
 
 const Footer = () => {
     return (
         <div>
+            <br/>
             <h1>
+
             </h1>
+            <p className='twitter-link'>
+                <Link to="https://twitter.com/CryptOptionsApp" target='_blank'> <BsTwitter/> </Link>
+            </p>
+            <p className='twitter-link'>
+                Follow us on <Link to="https://twitter.com/CryptOptionsApp" target='_blank'> <span><b>&nbsp;Twitter </b></span></Link>
+            </p>
             <p className={'version_tag'}>
                 Curious about this app ? <br/> Any feedback or feature request ? <br/><br/>
                 <b>Join us on<Link to="https://discord.gg/srYeUy9CcR" target='_blank'> Discord </Link> </b>
@@ -16,7 +26,7 @@ const Footer = () => {
                 Coin prices provided by <Link to="https://www.coingecko.com" target='_blank'> CoinGecko </Link>
             </p>
             <p className={'version_tag'}>
-                version 0.7.15 (beta)
+                version 0.7.16 (beta)
             </p>
         </div>
     )
