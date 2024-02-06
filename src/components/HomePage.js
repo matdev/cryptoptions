16e27770ea0e45c4762237a41eb9e2579c160e69
Matdev <mcawox@gmail.com>
2024-02-06 23:08:56 +0100

    Displays value of the CryptOptions Major coins index

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import CoinItem from './CoinItem'
 
 import './HomePage.css'
@@ -9,6 +9,8 @@ import DataGrid from "react-data-grid";
 import {useSelector} from "react-redux";
 import MailchimpSubscribe from "react-mailchimp-subscribe"
 import {useTranslation} from "react-i18next";
+import axios from "axios";
+import ReactGA from "react-ga4";
 
 const HomePage = (props) => {
 
@@ -16,7 +18,48 @@ const HomePage = (props) => {
 
     const mailchimp_url = "https://cryptoptions.us21.list-manage.com/subscribe/post?u=d48a8293e3b5cf0f0002e1dd7&amp;id=585fd685ef&amp;f_id=0098dee6f0";
 
-    const SimpleForm = () => <MailchimpSubscribe url={mailchimp_url}/>
+    const userCurrency = useSelector(store => store.userCurrency.value);
+
+    const [majorIndexValue, setMajorIndexValue] = useState(0);
+
+
+    const majorIndexWeight = [15, 70, 15];
+
+    //setMajorIndexValue(majorIndexVal);
+    //setMajorIndexValue(3.14);
+
+    console.log("majorIndexValue = " + majorIndexValue);
+
+    useEffect(() => {
+
+        let majorIndexVal = 0;
+
+        if (props.coins != null) {
+            props.coins.forEach(coin => {
+
+                let weight = 0;
+
+                if (coin.id == "bitcoin") {
+                    weight = majorIndexWeight[0];
+                } else if (coin.id == "ethereum") {
+                    weight = majorIndexWeight[1];
+                } else if (coin.id == "solana") {
+                    weight = majorIndexWeight[2];
+                } else {
+                    //return;
+                }
+
+                if (weight > 0) {
+                    console.log("coin.id = " + coin.id + " price = " + coin.current_price + " weight = " + weight);
+                    majorIndexVal = majorIndexVal + coin.current_price * (weight / 100);
+                }
+
+            });
+        }
+
+        setMajorIndexValue(majorIndexVal);
+
+    }, [userCurrency, i18n.language])
 
 // a basic form
     const CustomForm = ({status, message, onValidated}) => {
@@ -48,7 +91,8 @@ const HomePage = (props) => {
                 </div>
                 <br/>
 
-                {status === "sending" && <div className={"newsletter_subscription_success"}>{t("mailchimp_sending")}</div>}
+                {status === "sending" &&
+                    <div className={"newsletter_subscription_success"}>{t("mailchimp_sending")}</div>}
                 {status === "error" && (
                     <div
                         className={"newsletter_subscription_error"}
@@ -80,33 +124,44 @@ const HomePage = (props) => {
             <div className='header'>
                 <br/>
                 <h2 className='header_title'>{t("welcome")}</h2>
-                <h3 className='header_text'>{t("home_intro_1")}
-                    <br/>
-                    <br/>
-                    {t("home_intro_2")}
-                    <br/>
-                    <br/>
-                    {t("home_intro_3")}
-                    <br/>
+                <h3 className='header_text'>
+                    {t("home_intro_4")}
                 </h3>
+                <h3 className='header_text'>
 
-                <div className='header_text'>
-                    <MailchimpSubscribe
-                        url={mailchimp_url}
-                        render={({subscribe, status, message}) => (
-                            <CustomForm
-                                status={status}
-                                message={message}
-                                onValidated={formData => subscribe(formData)}
-                            />
-                        )}
-                    />
 
-                </div>
-
-                <h2 className='table_title'>{t("home_table_title")}</h2>
+                </h3>
             </div>
 
+            <h2 className='table_title'>{t("crypto_index_title")}</h2>
+            <div className='content'>
+                <div className='details_info'>
+                    <div className='coin-heading'>
+                        <div className='rank'>
+                            <div className='rank'>
+                                <span className='rank-btn'>{t("crypto_major_index_title")}</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='centered-in-cell'>
+                        <div className='coin-price'>
+                            <h1> {majorIndexValue.toFixed(2)}</h1>
+                        </div>
+
+                    </div>
+                </div>
+                <br/>
+                <div className='coin-heading'>
+                    <div className='rank'>
+
+                    </div>
+                </div>
+                <div className='centered-in-cell'>
+                    {t("crypto_major_index_desc")} {t("crypto_major_index_desc_2")}
+                </div>
+            </div>
+            <br/>
+            <h2 className='table_title'>{t("home_table_title")}</h2>
             <div>
                 <div className='heading'>
                     <p className='hide-mobile'>#</p>
@@ -149,6 +204,33 @@ const HomePage = (props) => {
                     </div>
                 }
             </div>
+            <div className='header'>
+                <br/>
+                <h2 className='header_title'></h2>
+                <h3 className='header_text'>
+                    {t("home_intro_1")}
+                    <br/>
+                    <br/>
+                    {t("home_intro_2")}
+                </h3>
+                <div className='header_text'>
+                    <MailchimpSubscribe
+                        url={mailchimp_url}
+                        render={({subscribe, status, message}) => (
+                            <CustomForm
+                                status={status}
+                                message={message}
+                                onValidated={formData => subscribe(formData)}
+                            />
+                        )}
+                    />
+                    <h3 className='header_text'>
+                        {t("home_intro_3")}
+                    </h3>
+                </div>
+            </div>
+
+
         </div>
     )
 }
