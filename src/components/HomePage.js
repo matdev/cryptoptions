a0cac6e76ff71a17fc52d2dce641df0b3780efee
Matdev <mcawox@gmail.com>
2024-02-08 00:02:25 +0100

    Fix value of the CryptOptions Major coins index

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -12,6 +12,8 @@ import {useTranslation} from "react-i18next";
 import axios from "axios";
 import ReactGA from "react-ga4";
 
+const mathjs = require('mathjs');
+
 const HomePage = (props) => {
 
     const {i18n, t} = useTranslation();
@@ -20,46 +22,176 @@ const HomePage = (props) => {
 
     const userCurrency = useSelector(store => store.userCurrency.value);
 
+    const full_history_length = 5; // Price history for vol calculation
+
+    const [lastKnownPrices, setLastKnownPrices] = useState([]);
+
     const [majorIndexValue, setMajorIndexValue] = useState(0);
 
+    const historiesLoaded = [false, false, false];
+    const historiesLoading = [false, false, false];
 
     const majorIndexWeight = [15, 70, 15];
 
-    //setMajorIndexValue(majorIndexVal);
-    //setMajorIndexValue(3.14);
+    const prop_coins = [];
+
+    const BITCOIN_INDEX = 0;
+    const ETHEREUM_INDEX = 1;
+    const SOLANA_INDEX = 2;
+
+    const coin1 = {
+        id: 'bitcoin',
+        symbol: 'btc'
+    };
+    prop_coins[BITCOIN_INDEX] = coin1;
+
+    const coin2 = {
+        id: 'ethereum',
+        symbol: 'eth'
+    };
+    prop_coins[ETHEREUM_INDEX] = coin2;
+
+    const coin3 = {
+        id: 'solana',
+        symbol: 'sol'
+    };
+
+    prop_coins[SOLANA_INDEX] = coin3;
+
+    const rawPriceHistory = useState([]);
 
     console.log("majorIndexValue = " + majorIndexValue);
 
     useEffect(() => {
 
-        let majorIndexVal = 0;
+        setLastKnownPrices([0, 0, 0]);
 
-        if (props.coins != null) {
-            props.coins.forEach(coin => {
+        let current_coin_index = 0;
+        doRequestPriceHistoryIfNecessary(current_coin_index);
 
-                let weight = 0;
+        current_coin_index = 1;
+        doRequestPriceHistoryIfNecessary(current_coin_index);
 
-                if (coin.id == "bitcoin") {
-                    weight = majorIndexWeight[0];
-                } else if (coin.id == "ethereum") {
-                    weight = majorIndexWeight[1];
-                } else if (coin.id == "solana") {
-                    weight = majorIndexWeight[2];
-                } else {
-                    //return;
-                }
-
-                if (weight > 0) {
-                    console.log("coin.id = " + coin.id + " price = " + coin.current_price + " weight = " + weight);
-                    majorIndexVal = majorIndexVal + coin.current_price * (weight / 100);
-                }
-
-            });
-        }
-
-        setMajorIndexValue(majorIndexVal);
+        current_coin_index = 2;
+        doRequestPriceHistoryIfNecessary(current_coin_index);
 
     }, [userCurrency, i18n.language])
+
+    function doRequestPriceHistoryIfNecessary(coin_index) {
+        if (!historiesLoaded[coin_index] && !historiesLoading[coin_index]) {
+            doRequestPriceHistory(coin_index);
+        } else {
+            //console.log("doRequestPriceHistoryIfNecessary() HISTORY ALREADY LOADED FOR " + prop_coins[coin_index].id);
+        }
+    }
+
+    const doRequestPriceHistory = function (coin_index) {
+
+        const price_history_url = 'https://api.coingecko.com/api/v3/coins/' + prop_coins[coin_index].id + '/market_chart?vs_currency=' + userCurrency.code + '&days=' + full_history_length + '&interval=daily';
+        historiesLoading[coin_index] = true;
+
+        axios.get(price_history_url).then((res) => {
+
+            historiesLoaded[coin_index] = true;
+            historiesLoading[coin_index] = false;
+
+            let pricesHistoryFromService = res.data.prices;
+
+            if (prop_coins[coin_index].symbol == "btc") {
+
+                console.log("HomePage.doRequestPriceHistory().get().then() RECEIVED BITCOIN HISTORY ! : " + prop_coins[coin_index].symbol + " SIZE = " + res.data.prices.length);
+                rawPriceHistory[BITCOIN_INDEX] = pricesHistoryFromService;
+
+            } else if (prop_coins[coin_index].symbol == "eth") {
+
+                console.log("HomePage.doRequestPriceHistory().get().then() RECEIVED ETHEREUM HISTORY ! : " + prop_coins[coin_index].symbol + " SIZE = " + res.data.prices.length);
+                rawPriceHistory[ETHEREUM_INDEX] = pricesHistoryFromService;
+
+            } else if (prop_coins[coin_index].symbol == "sol") {
+
+                console.log("HomePage.doRequestPriceHistory().get().then() RECEIVED SOLANA HISTORY ! : " + prop_coins[coin_index].symbol + " SIZE = " + res.data.prices.length);
+                rawPriceHistory[SOLANA_INDEX] = pricesHistoryFromService;
+            }
+
+            let pricesHistoryCoin = [];
+            let datesHistoryCoin = [];
+            let dailyReturnHistory = [];
+
+            let i = 0;
+            for (const entry of pricesHistoryFromService) {
+
+                if (entry[1] === undefined) {
+                    console.log("HomePage.doRequestPriceHistory().get().then() i=  " + i + " undefined ");
+                } else {
+
+                    datesHistoryCoin[i] = entry[0];
+                    pricesHistoryCoin[i] = entry[1];
+
+                    if (i > 0) {
+                        dailyReturnHistory[i - 1] = mathjs.log(pricesHistoryCoin[i] / pricesHistoryCoin[i - 1]);
+                    }
+                }
+
+                i = i + 1;
+            }
+
+            //No vol calc for now
+            // let standardDeviation = mathjs.std(pricesHistoryCoin);
+            // let histoVol = standardDeviation * mathjs.sqrt(365) * 100;
+
+            lastKnownPrices[coin_index] = pricesHistoryCoin[pricesHistoryCoin.length - 1];
+
+            console.log("HomePage.doRequestPriceHistory().get().then() coin_index = " + coin_index + " price = " + lastKnownPrices[coin_index]);
+
+            if (isAllHistoriesLoaded()) {
+                console.log("HomePage.doRequestPriceHistory().get().then() ALL INDEX COMPONENT PRICES LOADED !! => PROCEED to index calculation");
+                calcIndexes();
+            } else {
+                console.warn("HomePage.doRequestPriceHistory().get().then() MISSING INDEX COMPONENT PRICES : " + lastKnownPrices);
+                setMajorIndexValue(0);
+            }
+        }).catch((error) => {
+            setMajorIndexValue(0);
+            console.log(error);
+            historiesLoaded[coin_index] = false;
+            historiesLoading[coin_index] = false;
+        });
+    };
+
+    function calcIndexes() {
+
+        console.warn("calcIndexes() lastKnownPrices = " + lastKnownPrices);
+
+        let majorIndexVal = 0;
+
+        console.log("calcIndexes() Calc majorIndexVal : userCurrency = " + userCurrency);
+
+        majorIndexVal = majorIndexVal + lastKnownPrices[BITCOIN_INDEX] * (majorIndexWeight[BITCOIN_INDEX] / 100);
+        majorIndexVal = majorIndexVal + lastKnownPrices[ETHEREUM_INDEX] * (majorIndexWeight[ETHEREUM_INDEX] / 100);
+        majorIndexVal = majorIndexVal + lastKnownPrices[SOLANA_INDEX] * (majorIndexWeight[SOLANA_INDEX] / 100);
+
+        setMajorIndexValue(majorIndexVal);
+    }
+
+    function isAllHistoriesLoaded() {
+
+        for (let i = 0; i < historiesLoaded.length; i++) {
+            if (!historiesLoaded[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    function getMajorIndexValueAsString() {
+
+        if (majorIndexValue > 0) {
+            return majorIndexValue.toFixed(2);
+        } else {
+            return "Not available";
+        }
+    }
 
 // a basic form
     const CustomForm = ({status, message, onValidated}) => {
@@ -139,13 +271,13 @@ const HomePage = (props) => {
                     <div className='coin-heading'>
                         <div className='rank'>
                             <div className='rank'>
-                                <span className='rank-btn'>{t("crypto_major_index_title")}</span>
+                                <span className='rank-btn'>{t("crypto_major_index_title")} {userCurrency.symbol}</span>
                             </div>
                         </div>
                     </div>
                     <div className='centered-in-cell'>
                         <div className='coin-price'>
-                            <h1> {majorIndexValue.toFixed(2)}</h1>
+                            <h1> {getMajorIndexValueAsString()}</h1>
                         </div>
 
                     </div>
